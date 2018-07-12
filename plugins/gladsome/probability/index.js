1ab211b66b0f2af8b145a3aea2e144d7f010c3e6
Seia Soto <seriumium@outlook.kr>
2018-07-13 03:37:26 +0900

    Re:PROB

--- plugins/gladsome/probability/index.js
+++ plugins/gladsome/probability/index.js
@@ -3,5 +3,5 @@ module.exports.execute = (client, message, nt) => {
   const probability = Math.floor(Math.random() * 100) + 1
   const reason = nt.arguments.slice(0).join(' ')
   const translate = nt.i('probability', true)
-  message.channel.send(nt.i('percentageIs'), probability)
+  message.channel.send(nt.i('percentageIs').replace('{of}', reason).replace('percentage', probability))
 }
