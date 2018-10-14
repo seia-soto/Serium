566c534cf9f1f4421b79883de483d95bd386e6c9
Seia <30369714+Seia-Soto@users.noreply.github.com>
2018-10-14 22:13:41 +0900

    Changed the variable name because it has already taken.

--- plugins/gladsome/probability/index.js
+++ plugins/gladsome/probability/index.js
@@ -2,8 +2,8 @@ module.exports = (client, message, nt) => {
   const probability = Math.floor(Math.random() * 100) + 1
   const reason = nt.parameters.slice(0).join(' ')
   const translate = nt.i('probability', true)
-  const message = nt.i('percentageIs')
+  const result = nt.i('percentageIs')
     .replace('{of}', reason)
     .replace('{percentage}', probability)
-  message.channel.send(message)
+  message.channel.send(result)
 }
