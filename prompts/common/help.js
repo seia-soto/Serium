85a9d0ebeca68fd8e0b58f718a4019bc16e58993
Seia <seia@outlook.kr>
2019-04-13 16:40:33 +0900

    Update docs and guides

--- prompts/common/help.js
+++ prompts/common/help.js
@@ -1,32 +1,17 @@
 const prompts = require('../')
-const structures = require('../../structures')
-
-const {PreferenceIndicator} = structures
-
-// NOTE: Construct help embed once, not eventually.
-let helpEmbed = {
-  embed: {
-    title: 'Sewritten',
-    fields: [],
-    description: PreferenceIndicator.App.Externals.PromotionLines.description,
-    image: {
-      url: 'https://raw.githubusercontent.com/Seia-Soto/Serium/nightly/assets/images/banner.png'
-    }
-  }
-}
-prompts.forEach(prompt => {
-  if (helpEmbed.fields.filter(field => field.name === prompt.properties.category)) {
-    helpEmbed.fields[helpEmbed.fields.indexOf(prompt.properties.category)].value += '`' + prompt.properties.name + '` '
-  } else {
-    helpEmbed.fields.push({
-      title: prompt.properties.category,
-      value: '`' + prompt.properties.name + '` '
-    })
-  }
-})
+const PreferenceIndicator = require('../../structures/PreferenceIndicator')
 
 const Plan = (message, client) => {
-  message.channel.send(helpEmbed)
+
+  message.channel.send({
+    embed: {
+      title: 'Sewritten',
+      description: PreferenceIndicator.App.Externals.PromotionLines.description,
+      image: {
+        url: 'https://app.seia.io/Serium/assets/images/banner.png'
+      }
+    }
+  })
 }
 const Properties = {
   name: 'help',
