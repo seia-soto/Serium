fb2ea1a4a2a45d5e9270b1f1b63a7166358571e5
Seia <30369714+Seia-Soto@users.noreply.github.com>
2019-06-11 03:24:06 +0900

    Make customizing of random reward

--- flow/message/randomRewardCollector.js
+++ flow/message/randomRewardCollector.js
@@ -1,12 +1,14 @@
-const EndPreferenceIndicator = require('@structures/EndPreferenceIndicator')
+const structures = require('@structures')
+
+const {EndPreferenceIndicator, PreferenceIndicator} = structures
 
 const isCollective = (maxPercentage, accptablePercentage) => {
   return ((Math.round(Math.random() * maxPercentage) + 1) < accptablePercentage) ? true : false
 }
 const randomRewardCollector = message => {
-  if (isCollective(100, 2)) {
+  if (isCollective(100, PreferenceIndicator.Ecosystem.Economy.randomWages.percentage)) {
     EndPreferenceIndicator.getUserSettings(message.author.id).then(preference => {
-      preference.economy.shards += 2
+      preference.economy.shards += PreferenceIndicator.Ecosystem.Economy.randomWages.amount
 
       EndPreferenceIndicator.setUserSettings(message.author.id, preference)
     })
