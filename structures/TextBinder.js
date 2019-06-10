8453268f9ae1fdfd6a8747aabce05f5c83db941d
Seia <30369714+Seia-Soto@users.noreply.github.com>
2019-06-11 03:28:37 +0900

    Make text binder to make dynamic strings can store variables

---
+++ structures/TextBinder.js
@@ -0,0 +1,13 @@
+const TextBinder = (text, parameters) => {
+  if (!text) return ''
+
+  const keys = text.split(' ')
+    .filter(context => context)
+    .filter(key => key.match(/\{(.*?)\}/)) // NOTE: {key}
+    .map(key => key.match(/\{(.*?)\}/)[1]) // NOTE: key
+  keys.forEach(key => text = text.replace(`{${key}}`, parameters[key], 'g'))
+
+  return text
+}
+
+module.exports = TextBinder
---
+++ translates/ko/index.js
@@ -0,0 +1,1 @@
+module.exports.prompts = require('./prompts')
