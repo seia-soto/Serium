bf3ce1ee03dff02ff784e1e2e85f45daba42e5f9
Seia <30369714+Seia-Soto@users.noreply.github.com>
2019-07-03 13:46:43 +0900

    Fix scope

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,23 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-const categories = new Object()
+ const categories = new Object()
 
-categories.common = require('./common')
-categories.serium = require('./serium')
+ categories.common = require('./common')
+ categories.serium = require('./serium')
 
-Object.keys(categories).forEach(categoryName => {
-  const category = categories[categoryName]
+ Object.keys(categories).forEach(categoryName => {
+   const category = categories[categoryName]
 
-  Object.values(category).forEach(command => {
-    module.exports[command.properties.name] = command
-    module.exports[command.properties.name].properties.category = categoryName
+   Object.values(category).forEach(command => {
+     module.exports[command.properties.name] = command
+     module.exports[command.properties.name].properties.category = categoryName
 
-    (command.properties.aliases || []).forEach(alias => {
-      module.exports[alias] = command
-      module.exports[alias].properties.category = categoryName
-      // NOTE: Check this command is alias
-      module.exports[alias].properties.alias = true
-    })
-  })
-})
+     const aliases = command.properties.aliases || []
+
+     aliases.forEach(alias => module.exports[alias] = command)
+   })
+ })
